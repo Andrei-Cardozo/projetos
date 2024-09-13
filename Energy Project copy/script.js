c907ad38f1fe7052c03fb87180e0424ba580a78f
Andrei Cardozo <andrei.cardozo@unidavi.edu.br>
2024-09-13 09:06:25 -0300

    Adionando Novo Projeto

---
+++ Energy Project copy/script.js
@@ -0,0 +1,59 @@
+class MobileNavbar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navlist = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index)=>{
+            link.style.animation
+            ?(link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+            }s`);
+        });
+    }
+
+    handleClick(){
+        this.navlist.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
+
+function alterarFavicon(caminhoFavicon) {
+    var link = document.getElementById('projectenergy');
+        if (link) {
+            link.href = caminhoFavicon;
+        } else {
+            var novoLink = document.createElement('link');
+            novoLink.id = 'projectenergy';
+            novoLink.rel = 'icon';
+            novoLink.href = caminhoFavicon;
+            novoLink.type = 'imagem/x-icon'
+            document.head.appendChild(novoLink);
+        }
+}
+alterarFavicon('imagens/projectenergy.png');
